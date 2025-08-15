31ef2bc9993014e94e9616d87dc45406b62962a4
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-08-15 16:07:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+const submitBtn = document.getElementById("submit");
+const form = document.getElementById("loginForm");
+checkbox.checked = false;
+if (localStorage.getItem("userName") && localStorage.getItem("passWord"))
+  existingBtn.style.display = "block";
+
+function loginAndSave(userName, passWord) {
+  localStorage.setItem("userName", userName);
+  localStorage.setItem("passWord", passWord);
+  alert(`Logged in as ${userName}`);
+  existingBtn.style.display = "block";
+}
+function loginNoSave(userName) {
+  alert(`Logged in as ${userName}`);
+  localStorage.clear();
+  existingBtn.style.display = "none";
+}
+existingBtn.addEventListener("click", () => {
+  alert(`Loged in as ${localStorage.getItem("userName")}`);
+});
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const userName = username.value.trim();
+  const passWord = password.value.trim();
+  if (!userName || !passWord) {
+    alert("please enter username and password!");
+    return;
+  }
+  if (checkbox.checked) loginAndSave(userName, passWord);
+  else loginNoSave(userName);
+  form.reset();
+});
